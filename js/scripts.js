25ab183c9d9c3353b54778a67930788066992036
Mateusz Lawczys <ma6tek@gmail.com>
2017-11-23 20:51:13 +0100

    Add files, Add comments in js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+//Example comment
+
+// var name = prompt('Enter your name');  - line comment disabling this element of code
+/* var surname = prompt('And your surname !');
+alert('Hello, ' + name + ' ' + surname);
+console.log('Hello, ' + name + surname);  - block comment */
+var phone = prompt('Enter your phone number:');
+alert('You should never do this again, fool! xD');
+console.log('Fool\'s number: ' + phone);
